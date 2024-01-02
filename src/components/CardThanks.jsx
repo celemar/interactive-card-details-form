f5c20eb06ba8f5aea9f98b7ea50b3a493156986a
celemar <celemar@pm.me>
2024-01-02 16:12:56 -0300

    First Commit

--- src/components/CardThanks.jsx
+++ src/components/CardThanks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import iconComplete from "../assets/icon-complete.svg";
 
 const CardThanks = () => {
   const handleContinueClick = () => {
@@ -8,11 +9,7 @@ const CardThanks = () => {
   return (
     <div className="card-thanks">
       <div className="card-thanks__wrapper">
-        <img
-          className="icon-complete"
-          src="./src/assets/icon-complete.svg"
-          alt="icon complete"
-        />
+        <img className="icon-complete" src={iconComplete} alt="icon complete" />
         <h2>Thank You!</h2>
         <p>We've added your card details</p>
         <button onClick={handleContinueClick} aria-label="Continue">
