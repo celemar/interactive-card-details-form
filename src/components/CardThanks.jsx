b562a5ab24b45b43fce677957da2a7e9b477bd0e
celemar <celemar@pm.me>
2024-01-02 15:59:05 -0300

    First Commit

--- src/components/CardThanks.jsx
+++ src/components/CardThanks.jsx
@@ -8,7 +8,7 @@ const CardThanks = () => {
   return (
     <div className="card-thanks">
       <div className="card-thanks__wrapper">
-      <img className="icon-complete" src="src\assets\icon-complete.svg" alt="icon complete" />
+      <img className="icon-complete" src="src/assets/icon-complete.svg" alt="icon complete" />
       <h2>Thank You!</h2>
       <p>We've added your card details</p>
       <button onClick={handleContinueClick} aria-label="Continue">Continue</button>
